b73495eebe3de5b65a3b8aff68a7035ca05b70dd
edzh <edwinzzhou@gmail.com>
2019-03-01 10:43:01 -0500

    Add present control to students

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,8 +3,17 @@ import React from 'react';
 export default function LibraryList(props) {
   return (
     <div>
+      <h3>Library</h3>
       {props.library.map((student, index) => (
-        <div key={index}>{student.name}</div>
+        <div key={index} onClick={() => props.removeChildFromLibrary(student)}>
+          {student.present ? (
+            <p>{student.name}</p>
+          ) : (
+            <p>
+              <strike>{student.name}</strike>
+            </p>
+          )}
+        </div>
       ))}
     </div>
   );
