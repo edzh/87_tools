65a95320a205c8ebfa74e2106217efa4e621a8aa
Edwin Zhou <edwinzzhou@gmail.com>
2019-10-20 02:24:09 -0400

    converted to configure store

--- src/client/store.js
+++ src/client/store.js
@@ -1,9 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { configureStore } from 'redux-starter-kit';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+export const store = configureStore({
+  reducer: reducer,
+  middleware: [thunk]
+});
