849b0c5d0ffbd9992b691ab967cc4d93e1eb8218
edzh <edwinzzhou@gmail.com>
2020-01-22 13:07:07 -0500

    Hide attendance navbar for io and change timesheet route to default to timeclock

--- src/client/components/Timesheet/TimesheetButton.js
+++ src/client/components/Timesheet/TimesheetButton.js
@@ -5,7 +5,7 @@ export default ({ timesheet }) => {
   return (
     <Link
       className="mr-2 py-1 px-2 bg-gray-100 hover:bg-gray-200 rounded border border-gray-400"
-      to={`/timesheet/${timesheet._id}`}
+      to={`/timesheet/${timesheet._id}/timeclock`}
     >
       {`${timesheet.io === 'in' ? 'Sign in' : 'Sign out'}`}
     </Link>
--- src/client/features/timesheet/Navbar.js
+++ src/client/features/timesheet/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar({ timesheetId }) {
+  const timesheet = useSelector(state => state.timeclock.timesheets);
+
   return (
     <ul className="flex px-2 py-2 bg-white border border-gray-400 rounded">
       <li>
@@ -13,15 +16,17 @@ export default function Navbar({ timesheetId }) {
           Timeclock
         </NavLink>
       </li>
-      <li>
-        <NavLink
-          className="px-2 py-2 mx-1"
-          activeClassName="border-b-2 border-blue-400"
-          to={`/timesheet/${timesheetId}/attendance`}
-        >
-          Attendance
-        </NavLink>
-      </li>
+      {timesheet.byId[timesheetId].io === 'in' && (
+        <li>
+          <NavLink
+            className="px-2 py-2 mx-1"
+            activeClassName="border-b-2 border-blue-400"
+            to={`/timesheet/${timesheetId}/attendance`}
+          >
+            Attendance
+          </NavLink>
+        </li>
+      )}
     </ul>
   );
 }
