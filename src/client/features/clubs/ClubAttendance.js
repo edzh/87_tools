9dfe66654d3af765f4e40740a687e54999e86c30
edzh <edwinzzhou@gmail.com>
2020-03-04 13:52:06 +0800

    add live update to timestamp list

--- src/client/features/clubs/ClubAttendance.js
+++ src/client/features/clubs/ClubAttendance.js
@@ -13,11 +13,19 @@ export default function ClubAttendance({ match }) {
   const club = clubPage.item;
   const timestamps = useSelector(state => state.clubPage.timestamps);
   const timesheets = clubPage.timesheets;
-  const timesheetsByClubDay = timesheets.allIds.filter(
-    timesheetId =>
-      getDay(parseISO(timesheets.byId[timesheetId].date)) ===
-        +club.byId[club.allIds].day && timesheets.byId[timesheetId].io === 'in'
-  );
+  const timesheetsByClubDay = timesheets.allIds
+    .filter(timesheetId => {
+      const timesheetIsSignin = timesheets.byId[timesheetId].io === 'in';
+      const timesheetDay = getDay(parseISO(timesheets.byId[timesheetId].date));
+      const clubDay = +club.byId[club.allIds].day;
+
+      return timesheetDay === clubDay && timesheetIsSignin;
+    })
+    .sort(
+      (timesheetIdA, timesheetIdB) =>
+        timesheets.byId[timesheetIdA].date - timesheets.byId[timesheetIdB].date
+    );
+
   const students = useSelector(state => state.students.items);
   const session = useSelector(state => state.currentSession.item);
   const timestampsByClub = clubPage.timestamps;
@@ -45,31 +53,51 @@ export default function ClubAttendance({ match }) {
   if (!students.allIds.length) return null;
 
   return (
-    <div>
-      <select onChange={e => setTimesheet(e.target.value)}>
-        <option value="">---</option>
-        {timesheetsByClubDay.map(timesheetId => (
-          <option key={timesheetId} value={timesheetId}>
-            {format(
-              parseISO(timesheets.byId[timesheetId].date),
-              'MMM dd, yyyy'
-            )}
-          </option>
-        ))}
-      </select>
-      <div>
-        {clubPage.students.allIds.map(studentId => {
-          const isSignedIn = clubStudentsByTimesheet.indexOf(studentId) > -1;
+    <div className="bg-white border rounded shadow w-64">
+      <div className="px-4 pt-3 pb-1 border-b">
+        <label className="mr-2" htmlFor="timesheet">
+          Timesheet
+        </label>
+        <select
+          id="timesheet"
+          className="form-input"
+          onChange={e => setTimesheet(e.target.value)}
+        >
+          <option value="">---</option>
+          {timesheetsByClubDay.map(timesheetId => (
+            <option key={timesheetId} value={timesheetId}>
+              {format(
+                parseISO(timesheets.byId[timesheetId].date),
+                'MMM dd, yyyy'
+              )}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div
+        className={`${timesheet ? '' : 'bg-gray-100'} overflow-auto`}
+        style={{ height: '24rem' }}
+      >
+        {timesheet ? (
+          clubPage.students.allIds.map(studentId => {
+            const isSignedIn = clubStudentsByTimesheet.indexOf(studentId) > -1;
 
-          return (
-            <div
-              className={isSignedIn ? 'text-green-500' : 'text-red-500'}
-              key={studentId}
-            >
-              {students.byId[studentId].name}
-            </div>
-          );
-        })}
+            return (
+              <div
+                className={`${
+                  isSignedIn ? 'text-green-500' : 'text-red-500'
+                } px-4 py-1 border-b`}
+                key={studentId}
+              >
+                {students.byId[studentId].name}
+              </div>
+            );
+          })
+        ) : (
+          <div className="text-center p-4 text-gray-600">
+            Select a timesheet
+          </div>
+        )}
       </div>
     </div>
   );
