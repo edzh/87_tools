ac256c1fefd3b7afb0d71571078251a60e5e8578
edzh <edwinzzhou@gmail.com>
2020-03-13 22:59:55 +0800

    Style student page

--- src/client/components/Student/EditStudentClubs.js
+++ src/client/components/Student/EditStudentClubs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { Link } from 'react-router-dom';
 
@@ -21,6 +21,8 @@ export default function EditStudentClubs({
   getClubsBySession,
   updateCurrentStudent
 }) {
+  const [edit, setEdit] = useState(false);
+
   function findClubWithDay(clubDay) {
     if (!currentStudent.byId[currentStudent.allIds].currentClubs) return '';
     const club = currentStudent.byId[currentStudent.allIds].clubs
@@ -34,14 +36,26 @@ export default function EditStudentClubs({
   if (!sessions.allIds.length) return null;
 
   return (
-    <div>
-      <h2 className="text-xl font-bold my-2">Clubs</h2>
+    <div className="p-4 bg-white rounded border border-gray-400 mt-2">
+      <button
+        onClick={() => setEdit(!edit)}
+        className={`${
+          edit ? 'bg-blue-500 text-white' : 'bg-white'
+        } px-2 text-xs border rounded shadow border-gray-200`}
+      >
+        Edit
+      </button>
+      <h2 className="text-xl font-bold mb-2">
+        Clubs{' '}
+        {currentSession.item.allIds &&
+          `- ${currentSession.item.byId[currentSession.item.allIds].name}`}
+      </h2>
 
       <div className="flex">
         {sessions.allIds.map(sessionId => (
           <p
             key={sessionId}
-            className="p-2 mr-2 btn block"
+            className="p-2 mr-2 btn block hover:bg-blue-400 cursor-pointer"
             onClick={() => {
               getCurrentSession(sessionId);
               getClubsBySession(sessionId);
@@ -51,88 +65,92 @@ export default function EditStudentClubs({
           </p>
         ))}
       </div>
-      <div className="p-4">
-        {currentStudent.byId[currentStudent.allIds].clubs &&
-          currentStudent.byId[currentStudent.allIds].clubs
-            .filter(club => club.session === currentSession.item.allIds)
-            .map((club, index) => (
-              <div className="flex m-2" key={index}>
-                <p className="w-32 capitalize">{intToDay[club.day]}</p>
-                <Link className="no-underline" to={`/club/${club._id}`}>
-                  <p className="text-blue-600  hover:text-blue">{club.name}</p>
-                </Link>
-              </div>
-            ))}
-      </div>
+      {edit ? (
+        currentSession.item.allIds && (
+          <Formik
+            enableReinitialize
+            initialValues={{
+              monday: findClubWithDay(1),
+              tuesday: findClubWithDay(2),
+              wednesday: findClubWithDay(3),
+              thursday: findClubWithDay(4),
+              friday: findClubWithDay(5)
+            }}
+            onSubmit={(values, actions) => {
+              const currentClubIds = [
+                values.monday,
+                values.tuesday,
+                values.wednesday,
+                values.thursday,
+                values.friday
+              ].filter(club => club !== '');
 
-      {currentSession.item.allIds && (
-        <Formik
-          enableReinitialize
-          initialValues={{
-            monday: findClubWithDay(1),
-            tuesday: findClubWithDay(2),
-            wednesday: findClubWithDay(3),
-            thursday: findClubWithDay(4),
-            friday: findClubWithDay(5)
-          }}
-          onSubmit={(values, actions) => {
-            const currentClubIds = [
-              values.monday,
-              values.tuesday,
-              values.wednesday,
-              values.thursday,
-              values.friday
-            ].filter(club => club !== '');
+              const sessionClubIds = currentStudent.byId[
+                currentStudent.allIds
+              ].clubs
+                .filter(club => club.session !== currentSession.item.allIds)
+                .map(club => club._id);
 
-            const sessionClubIds = currentStudent.byId[
-              currentStudent.allIds
-            ].clubs
-              .filter(club => club.session !== currentSession.item.allIds)
-              .map(club => club._id);
+              const clubs = Array.from(
+                new Set([...sessionClubIds, ...currentClubIds])
+              ).filter(club => club !== '');
 
-            const clubs = Array.from(
-              new Set([...sessionClubIds, ...currentClubIds])
-            ).filter(club => club !== '');
-
-            updateCurrentStudent({
-              ...currentStudent.byId[currentStudent.allIds],
-              currentClubIds,
-              clubs
-            });
-          }}
-        >
-          {() => (
-            <Form>
-              {intToDay.map((day, index) => (
-                <div className="flex" key={day}>
-                  <label
-                    className="my-auto font-bold w-24 capitalize"
-                    htmlFor={day}
-                  >
-                    {day}
-                  </label>
-                  <Field
-                    className="block p-2 rounded border border-gray-400 my-2"
-                    name={day}
-                    component="select"
-                  >
-                    <option value="">---</option>
-                    {clubs.allIds
-                      .filter(clubId => clubs.byId[clubId].day === index)
-                      .map(clubId => (
-                        <option className="block" key={clubId} value={clubId}>
-                          {clubs.byId[clubId].name}
-                        </option>
-                      ))}
-                  </Field>
+              updateCurrentStudent({
+                ...currentStudent.byId[currentStudent.allIds],
+                currentClubIds,
+                clubs
+              });
+            }}
+          >
+            {() => (
+              <Form className="my-4">
+                {intToDay.map((day, index) => (
+                  <div className="flex" key={day}>
+                    <label
+                      className="my-auto font-bold w-24 capitalize"
+                      htmlFor={day}
+                    >
+                      {day}
+                    </label>
+                    <Field
+                      className="block p-1 rounded border border-gray-400 mb-1"
+                      name={day}
+                      component="select"
+                    >
+                      <option value="">---</option>
+                      {clubs.allIds
+                        .filter(clubId => clubs.byId[clubId].day === index)
+                        .map(clubId => (
+                          <option className="block" key={clubId} value={clubId}>
+                            {clubs.byId[clubId].name}
+                          </option>
+                        ))}
+                    </Field>
+                  </div>
+                ))}
+                <button className="btn block hover:bg-blue-400" type="submit">
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
+        )
+      ) : (
+        <div className="my-4">
+          {currentStudent.byId[currentStudent.allIds].clubs &&
+            currentStudent.byId[currentStudent.allIds].clubs
+              .filter(club => club.session === currentSession.item.allIds)
+              .map((club, index) => (
+                <div className="flex my-2" key={index}>
+                  <p className="w-24 capitalize">{intToDay[club.day]}</p>
+                  <Link className="no-underline" to={`/club/${club._id}`}>
+                    <p className="text-blue-500 hover:text-blue-400">
+                      {club.name}
+                    </p>
+                  </Link>
                 </div>
               ))}
-              <button className="btn block hover:bg-blue-400" type="submit">
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
+        </div>
       )}
     </div>
   );
--- src/client/components/Student/StudentDetails.js
+++ src/client/components/Student/StudentDetails.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { Link } from 'react-router-dom';
+import Modal from 'client/components/Modal';
 
 const intToGrade = [
   'Kindergarten',
@@ -16,79 +17,103 @@ export default function StudentDetails({
   updateCurrentStudent
 }) {
   const [edit, setEdit] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   if (!currentStudent.allIds) return null;
 
   return (
-    <div className="">
-      <div className="flex">
+    <div className="mt-2">
+      <div className="bg-white p-4 rounded border border-gray-400">
         <button
           className={`${
             edit ? 'bg-blue-500 text-white' : 'bg-white'
-          } ml-auto text-xs border rounded shadow p-1`}
+          } ml-auto text-xs border rounded shadow px-2 mb-2`}
           onClick={() => setEdit(!edit)}
         >
           Edit
         </button>
-      </div>
-      <div className="bg-gray-100">
-        <div className="m-4 flex">
-          <h3 className="w-32 text-xl">Grade</h3>
-          <p className="text-xl">
-            {intToGrade[currentStudent.byId[currentStudent.allIds].grade]}
-          </p>
-        </div>
-        <div className="m-4 flex">
-          <h3 className="w-32 text-xl">PIN</h3>
-
-          {edit ? (
-            <Formik
-              initialValues={{
-                studentName: currentStudent.byId[currentStudent.allIds].name,
-                grade: currentStudent.byId[currentStudent.allIds].grade,
-                pin: currentStudent.byId[currentStudent.allIds].pin
-              }}
-              onSubmit={values => {
-                updateCurrentStudent({
-                  ...currentStudent.byId[currentStudent.allIds],
-                  name: values.studentName,
-                  grade: values.grade,
-                  pin: values.pin
-                });
-                setEdit(false);
-              }}
-            >
-              {() => (
-                <Form>
-                  <Field name="studentName" className="border rounded" />
-                  <Field
-                    component="select"
-                    name="grade"
-                    className="border rounded"
-                  >
-                    <option value="0">Kindergarten</option>
-                    <option value="1">1st Grade</option>
-                    <option value="2">2nd Grade</option>
-                    <option value="3">3rd Grade</option>
-                    <option value="4">4th Grade</option>
-                    <option value="5">5th Grade</option>
-                  </Field>
-                  <Field name="pin" className="border rounded" />
-                  <button type="submit">Submit</button>
-                </Form>
-              )}
-            </Formik>
-          ) : (
-            <p className="text-xl">
-              {currentStudent.byId[currentStudent.allIds].pin}
+        {edit ? (
+          <Formik
+            initialValues={{
+              studentName: currentStudent.byId[currentStudent.allIds].name,
+              grade: currentStudent.byId[currentStudent.allIds].grade,
+              pin: currentStudent.byId[currentStudent.allIds].pin
+            }}
+            onSubmit={values => {
+              updateCurrentStudent({
+                ...currentStudent.byId[currentStudent.allIds],
+                name: values.studentName,
+                grade: values.grade,
+                pin: values.pin
+              });
+              setEdit(false);
+            }}
+          >
+            {() => (
+              <Form>
+                <Field
+                  name="studentName"
+                  className="border rounded block mb-2"
+                />
+                <Field
+                  component="select"
+                  name="grade"
+                  className="border rounded block mb-2"
+                >
+                  <option value="0">Kindergarten</option>
+                  <option value="1">1st Grade</option>
+                  <option value="2">2nd Grade</option>
+                  <option value="3">3rd Grade</option>
+                  <option value="4">4th Grade</option>
+                  <option value="5">5th Grade</option>
+                </Field>
+                <Field name="pin" className="border rounded block mb-2" />
+                <button type="submit" className="btn p-1">
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
+        ) : (
+          <div>
+            <p className="text-xl mb-2">
+              {intToGrade[currentStudent.byId[currentStudent.allIds].grade]}
             </p>
-          )}
-        </div>
-        <button
-          onClick={() => deleteCurrentStudent(currentStudent.allIds)}
-          className="bg-red-500 text-white rounded p-2"
-        >
-          Delete
-        </button>
+            <div className="flex mb-2">
+              <h3 className="w-10 text-xl font-semibold">PIN</h3>
+              <p className="text-xl">
+                {currentStudent.byId[currentStudent.allIds].pin}
+              </p>
+            </div>
+            <button
+              onClick={() => setShowModal(true)}
+              className="bg-red-500 text-white rounded px-1 hover:bg-red-400"
+            >
+              Delete
+            </button>
+            {showModal ? (
+              <Modal>
+                <div className="mb-4">
+                  Are you sure you want to delete{' '}
+                  {currentStudent.byId[currentStudent.allIds].name}?
+                </div>
+                <div className="flex">
+                  <button
+                    onClick={() => deleteCurrentStudent(currentStudent.allIds)}
+                    className="bg-red-500 hover:bg-red-400 text-white rounded p-2 mr-2 flex-grow"
+                  >
+                    Yes
+                  </button>
+                  <button
+                    onClick={() => setShowModal(false)}
+                    className="bg-blue-500 hover:bg-blue-400 text-white rounded p-2 ml-2 flex-grow"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Modal>
+            ) : null}
+          </div>
+        )}
       </div>
     </div>
   );
